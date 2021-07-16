a29269f79039a1fefe0e0b179604b61bd1e4c21f
cheerio-pixel <frailin300@gmail.com>
2021-07-16 12:20:01 -0400

    For now, it doesn't have any sense to upgrade to v2

--- init.el
+++ init.el
@@ -504,21 +504,20 @@
   (org-superstar-configure-like-org-bullets)
   (setq org-superstar-headline-bullets-list '(?◉ ?⭆ ?○ ?✸ ?✿ ?✥ ?❂ ?❄ ?⁋))
   )
-;; Migration to org roam v2
-;; (use-package org-roam-server
-;;   :ensure t
-;;   :config
-;;   (setq org-roam-server-host "127.0.0.1"
-;;         org-roam-server-port 8080
-;;         org-roam-server-authenticate nil
-;;         org-roam-server-export-inline-images t
-;;         org-roam-server-serve-files nil
-;;         org-roam-server-served-file-extensions '("pdf" "mp4" "ogv")
-;;         org-roam-server-network-poll t
-;;         org-roam-server-network-arrows nil
-;;         org-roam-server-network-label-truncate t
-;;         org-roam-server-network-label-truncate-length 60
-;;         org-roam-server-network-label-wrap-length 20))
+(use-package org-roam-server
+  :ensure t
+  :config
+  (setq org-roam-server-host "127.0.0.1"
+        org-roam-server-port 8080
+        org-roam-server-authenticate nil
+        org-roam-server-export-inline-images t
+        org-roam-server-serve-files nil
+        org-roam-server-served-file-extensions '("pdf" "mp4" "ogv")
+        org-roam-server-network-poll t
+        org-roam-server-network-arrows nil
+        org-roam-server-network-label-truncate t
+        org-roam-server-network-label-truncate-length 60
+        org-roam-server-network-label-wrap-length 20))
 
 (use-package org-roam
   :ensure t
@@ -528,6 +527,7 @@
   :hook
   (after-init . org-roam-mode)
   (after-init . winner-mode)
+  (org-mode . nroam-mode)
   :custom
   (org-roam-graph-viewer "/usr/bin/google-chrome-stable")
   (org-roam-graph-exclude-matcher '("daily"))
