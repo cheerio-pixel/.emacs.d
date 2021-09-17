b9069747335746fd68c32b267b88784156df0227
cheerio-pixel <frailin300@gmail.com>
2021-09-17 09:30:55 -0400

    refactor(init): Disable commented out packages

--- init.el
+++ init.el
@@ -107,22 +107,23 @@
 
 (use-package vterm
   :straight t)
-;; (use-package eaf
-;;   :load-path "~/.emacs.d/site-lisp/emacs-application-framework" ; Set to "/usr/share/emacs/site-lisp/eaf" if installed from AUR
-;;   :init
-;;   (use-package epc :defer t :ensure t)
-;;   (use-package ctable :defer t :ensure t)
-;;   (use-package deferred :defer t :ensure t)
-;;   (use-package s :defer t :ensure t)
-;;   :custom
-;;   (eaf-browser-continue-where-left-off t)
-;;   :config
-;;   (eaf-setq eaf-browser-enable-adblocker "true")
-;;   (eaf-bind-key scroll_up "C-n" eaf-pdf-viewer-keybinding)
-;;   (eaf-bind-key scroll_down "C-p" eaf-pdf-viewer-keybinding)
-;;   (eaf-bind-key take_photo "p" eaf-camera-keybinding)
-;;   (eaf-bind-key nil "M-q" eaf-browser-keybinding) ;; unbind, see more in the Wiki
-;;   )
+(use-package eaf
+  :disabled
+  :load-path "~/.emacs.d/site-lisp/emacs-application-framework" ; Set to "/usr/share/emacs/site-lisp/eaf" if installed from AUR
+  :init
+  (use-package epc :defer t :ensure t)
+  (use-package ctable :defer t :ensure t)
+  (use-package deferred :defer t :ensure t)
+  (use-package s :defer t :ensure t)
+  :custom
+  (eaf-browser-continue-where-left-off t)
+  :config
+  (eaf-setq eaf-browser-enable-adblocker "true")
+  (eaf-bind-key scroll_up "C-n" eaf-pdf-viewer-keybinding)
+  (eaf-bind-key scroll_down "C-p" eaf-pdf-viewer-keybinding)
+  (eaf-bind-key take_photo "p" eaf-camera-keybinding)
+  (eaf-bind-key nil "M-q" eaf-browser-keybinding) ;; unbind, see more in the Wiki
+  )
 (use-package lispy
   :straight t
   :defer t
@@ -243,9 +244,10 @@
   ((python-mode . lsp)
    (python-mode . (lambda () (require 'lsp-pyright))))
   )
-;; (use-package realgud
-;;   :straight t
-;;   )
+(use-package realgud
+  :disabled
+  :straight t
+  )
 (use-package lsp-java
   :straight t
   :after lsp-mode
