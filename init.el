c5b29b2499b42168cff8266a856d601e201ce42a
cheerio-pixel <frailin300@gmail.com>
2021-09-21 19:16:07 -0400

    feat(init.pdf-tools): New package

--- init.el
+++ init.el
@@ -123,6 +123,8 @@
   ("U" pdf-view-previous-page)
   :init
   (use-package org-noter :custom (org-noter-notes-window-location 'other-frame))
+  ;; It simply needs to exist, since it extends saveplace
+  (use-package saveplace-pdf-view)
   :config
   ;; Don't make the daemon fail when a new version is avalible
   (ignore-errors
