0c990b26fdfbf66d4fd2168667082c884aad3655
cheerio-pixel <frailin300@gmail.com>
2022-08-05 17:54:33 -0400

    Bump custom file

--- custom-file.el
+++ custom-file.el
@@ -3,13 +3,23 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(org-modules
+   '(ol-bbdb ol-bibtex ol-docview ol-doi ol-eww ol-gnus ol-info ol-irc ol-mhe ol-rmail ol-w3m org-checklist))
  '(safe-local-variable-values
-   '((eval progn
-           (pp-buffer)
-           (indent-buffer))
-     (eval add-to-list 'load-path
+   '((eval add-to-list 'load-path
            (file-name-directory
-            (buffer-file-name))))))
+            (buffer-file-name)))
+     (elisp-lint-indent-specs
+      (describe . 1)
+      (it . 1)
+      (org-element-map . defun)
+      (org-roam-dolist-with-progress . 2)
+      (org-roam-with-temp-buffer . 1)
+      (org-with-point-at . 1)
+      (magit-insert-section . defun)
+      (magit-section-case . 0)
+      (org-roam-with-file . 2))
+     (elisp-lint-ignored-validators "byte-compile" "package-lint"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
