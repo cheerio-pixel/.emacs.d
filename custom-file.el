2ceb55778c8c9df7615093a011a1b43c6059ab25
cheerio-pixel <frailin300@gmail.com>
2022-08-08 07:28:17 -0400

    custom-file.el

--- custom-file.el
+++ custom-file.el
@@ -26,11 +26,9 @@
      (elisp-lint-ignored-validators "byte-compile" "package-lint"))))
 =======
  )
->>>>>>> ee5327b (custom-file.el)
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Fantasque Sans Mono" :foundry "outline" :slant normal :weight normal :height 130 :width normal))))
- '(cursor ((t (:background "white")))))
+ '(default ((t (:family "Fantasque Sans Mono" :foundry "outline" :slant normal :weight normal :height 130 :width normal)))))
