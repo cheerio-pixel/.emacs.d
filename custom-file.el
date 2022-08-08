2cd156d7aea18e3518382e03ea03a38ac9deba71
cheerio-pixel <frailin300@gmail.com>
2022-08-08 08:42:58 -0400

    custom-file.el

--- custom-file.el
+++ custom-file.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+<<<<<<< HEAD
  '(org-modules
    '(ol-bbdb ol-bibtex ol-docview ol-doi ol-eww ol-gnus ol-info ol-irc ol-mhe ol-rmail ol-w3m org-checklist))
  '(safe-local-variable-values
@@ -23,6 +24,8 @@
       (magit-section-case . 0)
       (org-roam-with-file . 2))
      (elisp-lint-ignored-validators "byte-compile" "package-lint"))))
+=======
+ )
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
